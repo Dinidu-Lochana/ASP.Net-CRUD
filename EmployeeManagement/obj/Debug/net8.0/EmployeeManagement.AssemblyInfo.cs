3be46ba8406d6ab4f5cafa2d562c3625fcd5781c
Dinidu-Lochana <24.dinidulochana@gmail.com>
2024-10-08 10:32:52 +0530

    CRUD

--- EmployeeManagement/obj/Debug/net8.0/EmployeeManagement.AssemblyInfo.cs
+++ EmployeeManagement/obj/Debug/net8.0/EmployeeManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5636a04b549238bc31a34d6ff4e995e3aa9b406a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77784b1126c542dfcb2f7e27aeb212bc36c50e2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
